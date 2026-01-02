1d81789be2b02e89ed9add91b574f7219ea498a6
TonyMcrae <tony.mcrae@outlook.com>
2026-01-02 15:35:51 -0500

    separating json parsing into its own file

---
+++ parsejson.py
@@ -0,0 +1,9 @@
+import json
+from pathlib import Path
+
+def load_json(json_path: items_key):
+    file = Path(json_path).open("r", encoding="utf-8")
+    data = json.load(file)
+    file.close()
+    items = data[items_key]
+    return items
